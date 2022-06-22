740aabf53e16e2252553c811ce37657084f48664
Emery <emeryhollar@gmail.com>
2022-06-22 15:11:20 -0400

    Updated Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,20 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Step 6: Using summary() determine the p-value and the r-squared value for the model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df))
+
+
+# Deliverable 2
+knitr::opts_chunk$set(echo = TRUE)
+library(tidyverse)
+data(diamonds, package='ggplot2')
+library(dplyr) 
+# Step 2 reading in the dataset
+suspension_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+head(suspension_table)
+
+# Step 3 creating a total summary
+?summarize
+total_summary <- suspension_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# Step 4 creating a lot summary. Groupby lot and summarize
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
